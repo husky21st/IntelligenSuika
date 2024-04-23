72c7d9e8c4fc20d6789c90561aef1daaef581ac6
Husky21st <husky21.mn@gmail.com>
2024-04-24 03:24:55 +0900

    [fix] ytl-dp file rename

--- materials/dl_movies.sh
+++ materials/dl_movies.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
- ./yt-dlp_macos --match-title "【スイカゲーム】" -f 136 -o './movies/%(title)s.%(ext)s' $1
+ ./yt-dlp --match-title "【スイカゲーム】" -f 136 -o './movies/%(title)s.%(ext)s' $1
 
-# ./yt-dlp_macos --match-title "【スイカゲーム】" -f "[ext=mp4][resolution=1280x720][fps=30]" -o './movies/%(title)s.%(ext)s' $1
+# ./yt-dlp --match-title "【スイカゲーム】" -f "[ext=mp4][resolution=1280x720][fps=30]" -o './movies/%(title)s.%(ext)s' $1
